c506e6b81c944022770c8be31ab445876996486c
Andreiutz <53002977+Andreiutz@users.noreply.github.com>
2022-02-18 16:39:58 +0200

    Cell Drawings

--- src/main/java/com/example/sudoku_fx_gui/HelloController.java
+++ src/main/java/com/example/sudoku_fx_gui/HelloController.java
@@ -7,8 +7,11 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class HelloController {
 
@@ -17,27 +20,33 @@ public class HelloController {
     private final TextField[][] textFields = new TextField[9][9];
     private final Integer[][] initialBoard = new Integer[9][9];
 
+    private Button solveButton;
+    private Button resetButton;
+
     public void setUpGrid(GridPane grid) {
         grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setHgap(5);
+        grid.setVgap(5);
+        grid.setPadding(new Insets(5, 5, 5, 5));
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 TextField t = new TextField();
                 t.setAlignment(Pos.CENTER);
                 textFields[i][j] = t;
-                t.setMinWidth(30);
-                t.setPrefWidth(30);
-                t.setMaxWidth(30);
 
-                t.setMinHeight(30);
-                t.setPrefHeight(30);
-                t.setMaxHeight(30);
+                t.setMinWidth(40);
+                t.setPrefWidth(40);
+                t.setMaxWidth(40);
+                int id = i*10+j;
+                t.setId(""+id);
 
-                t.setStyle("-fx-background-color: white");
+                t.setMinHeight(40);
+                t.setPrefHeight(40);
+                t.setMaxHeight(40);
 
+                t.setStyle("-fx-background-color: white;");
+                t.setFont(Font.font("", FontWeight.BOLD, 20));
                 t.setOnKeyTyped(new EventHandler<KeyEvent>() {
                     @Override
                     public void handle(KeyEvent keyEvent) {
@@ -61,6 +70,8 @@ public class HelloController {
                             }
                             else if (leadsToSolution()) {
                                 t.setStyle("-fx-background-color: lightgreen");
+                                //check if board completed
+                                if (checkForCompleteBoard()) finalEffect();
                             } else {
                                 t.setStyle("-fx-background-color: red");
                             }
@@ -68,12 +79,23 @@ public class HelloController {
                     }
                 });
 
+                t.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent mouseEvent) {
+                        //it has i and j coordinates
+                        //draw the 3x3 square it belongs to
+
+
+                        drawRelatedField(t);
+
+                    }
+                });
+
                 grid.add(t, j, i);
             }
         }
 
-        //Clear Button Setup
-        setupClearButton(grid);
+
 
         //Reset Button Setup
         setUpResetButton(grid);
@@ -81,37 +103,59 @@ public class HelloController {
         //Solve Button Setup
         setupSolveButton(grid);
 
+
+
         generateBoard();
 
     }
 
-    private void setupClearButton(GridPane grid) {
-        //Clear Button Setup
-        Button clearButton = new Button("Clear");
-        HBox hBox = new HBox(10);
-        hBox.getChildren().add(clearButton);
-        hBox.setAlignment(Pos.CENTER);
-        grid.add(hBox, 11, 4, 3, 1);
 
-        clearButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                for (int i = 0; i < 9; i++) {
-                    for (int j = 0; j < 9; j++) {
-                        textFields[i][j].setText("");
-                        textFields[i][j].setStyle("-fx-background-color: white");
-                    }
+
+
+    private void drawRelatedField(TextField t) {
+        int id = Integer.parseInt(t.getId().toString());
+        int x = id / 10;
+        int y = id % 10;
+        int xStart = x / 3 * 3;
+        int yStart = y / 3 * 3;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (i == x || y == j || (i >= xStart && i <= xStart+2 && j >= yStart && j <= yStart+2)) {
+                    textFields[i][j].setStyle("-fx-background-color: lightgray");
+                } else {
+                    textFields[i][j].setStyle("-fx-background-color: white");
                 }
             }
-        });
+        }
     }
 
+
+    private boolean checkForCompleteBoard() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (textFields[i][j].getText().trim().equals("")) return false;
+            }
+        }
+        return true;
+    }
+
+    private void finalEffect()  {
+
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    textFields[i][j].setStyle("-fx-background-color: lightgreen");
+                }
+            }
+
+    }
+
+
     private void setUpResetButton(GridPane grid) {
-        Button resetButton = new Button("Reset");
-        HBox hBox3 = new HBox(10);
+        resetButton = new Button("Reset");
+        HBox hBox3 = new HBox(5);
         hBox3.getChildren().add(resetButton);
         hBox3.setAlignment(Pos.CENTER);
-        grid.add(hBox3, 11, 5, 3, 1);
+        grid.add(hBox3, 11, 3, 3, 1);
 
         resetButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -125,11 +169,11 @@ public class HelloController {
 
     private void setupSolveButton(GridPane grid) {
         //Solve Button Setup
-        Button solveButton = new Button("Solve");
-        HBox hBox2 = new HBox(10);
+        solveButton = new Button("Solve");
+        HBox hBox2 = new HBox(5);
         hBox2.getChildren().add(solveButton);
         hBox2.setAlignment(Pos.CENTER);
-        grid.add(hBox2, 11, 6, 3, 1);
+        grid.add(hBox2, 11, 5, 3, 1);
 
         solveButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
